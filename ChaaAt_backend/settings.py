cf2e7a9b15bbb5a41a1513617b0db4e4366413a0
TheNightmareX <TheNightmareX@foxmail.com>
2020-12-08 01:16:07 +0800

    Throttling.

--- ChaaAt_backend/settings.py
+++ ChaaAt_backend/settings.py
@@ -37,6 +37,14 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 100,
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle',
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '3/second',
+        'user': '10/second',
+    }
 }
 
 # Application definition
